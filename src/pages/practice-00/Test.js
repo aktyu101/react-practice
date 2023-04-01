26a3ac552b2811412d71597956ec94f787a16e75
aktyu101 <aktyu101@gmail.com>
2023-04-02 01:26:07 +0900

    230402 : sutdy

--- src/pages/practice-00/Test.js
+++ src/pages/practice-00/Test.js
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import { Link, Outlet } from "react-router-dom";
+
 export default function Test() {
   const StudyList = styled.li`
     border: solid 1px #eee;
@@ -10,8 +11,12 @@ export default function Test() {
   return (
     <>
       <div id="content_wrapper">
-        <ul style={{ display: "flex" }}>
-          <Link to="/test/test01" className="nav_tab">
+        <ul style={{ display: "flex", gap: "5px" }}>
+          <Link
+            to="/test/test01"
+            style={{ border: "solid 1px #ddd", padding: "5px" }}
+            className="nav_tab"
+          >
             230326 : study
           </Link>
         </ul>
--- src/pages/practice-00/Test01.js
+++ src/pages/practice-00/Test01.js
@@ -1,7 +1,35 @@
+import styled from "@emotion/styled";
 export default function Test01() {
+  //   const [color1, setColor1] = useState(null);
+  //css
+  const Box = styled.li`
+    width: 200px;
+    height: 200px;
+    border: solid 1px #ddd;
+    box-sizing: border-box;
+  `;
   return (
     <>
-      <div>ddd</div>
+      <ul
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          width: "600px",
+          margin: "60px auto 0",
+          cursor: "pointer",
+          backgroundColor: "#eee",
+        }}
+      >
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+      </ul>
     </>
   );
 }
