c4a19cc7511bb26c63ec1e3f58c39044ba403a65
aktyu101 <aktyu101@gmail.com>
2023-04-06 20:05:31 +0900

    230406 : study

--- src/pages/practice-00/Test01.js
+++ src/pages/practice-00/Test01.js
@@ -1,13 +1,23 @@
 import styled from "@emotion/styled";
+import { useState } from "react";
+import { colors } from "./Color";
+import { ClassNames } from "@emotion/react";
+
 export default function Test01() {
-  //   const [color1, setColor1] = useState(null);
-  //css
+  const [color1, setColor1] = useState(null);
   const Box = styled.li`
     width: 200px;
     height: 200px;
     border: solid 1px #ddd;
     box-sizing: border-box;
   `;
+  console.log(colors);
+  const onChangeColor1 = () => {
+    setColor1((color) => {
+      return color === null ? "blue" : null;
+    });
+  };
+  console.log("box", Box);
   return (
     <>
       <ul
@@ -17,10 +27,14 @@ export default function Test01() {
           width: "600px",
           margin: "60px auto 0",
           cursor: "pointer",
-          backgroundColor: "#eee",
+          backgroundColor: colors[0],
         }}
       >
-        <Box></Box>
+        <Box
+          className={"name"}
+          onClick={onChangeColor1}
+          style={{ backgroundColor: color1 }}
+        ></Box>
         <Box></Box>
         <Box></Box>
         <Box></Box>
