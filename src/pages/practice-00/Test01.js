a2fee3b5a0269eef8017bb1e1e0f8acf2d406947
aktyu101 <aktyu101@gmail.com>
2023-04-08 21:30:08 +0900

    230408 : study

--- src/pages/practice-00/Test01.js
+++ src/pages/practice-00/Test01.js
@@ -1,49 +1,53 @@
 import styled from "@emotion/styled";
 import { useState } from "react";
-import { colors } from "./Color";
 import { ClassNames } from "@emotion/react";
 
-export default function Test01() {
-  const [color1, setColor1] = useState(null);
+export default function Test01(props) {
+  const [color, setColor] = useState("lightgrey");
   const Box = styled.li`
     width: 200px;
     height: 200px;
     border: solid 1px #ddd;
     box-sizing: border-box;
+    backgroundcolor: boxList.color[1];
   `;
-  console.log(colors);
+
+  const boxList = [
+    { id: 1, color: "red" },
+    { id: 2, color: "orange" },
+    { id: 3, color: "yellow" },
+    { id: 4, color: "green" },
+    { id: 5, color: "blue" },
+    { id: 6, color: "indigo" },
+    { id: 7, color: "pupple" },
+    { id: 8, color: "black" },
+    { id: 9, color: "pink" },
+  ];
+
   const onChangeColor1 = () => {
-    setColor1((color) => {
-      return color === null ? "blue" : null;
+    let i = 0;
+    const clickColor = boxList[i].color;
+    setColor((color) => {
+      return color === "lightgrey" ? boxList[i].color : "lightgrey";
     });
+    i++;
   };
-  console.log("box", Box);
+
   return (
-    <>
-      <ul
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          width: "600px",
-          margin: "60px auto 0",
-          cursor: "pointer",
-          backgroundColor: colors[0],
-        }}
-      >
-        <Box
-          className={"name"}
-          onClick={onChangeColor1}
-          style={{ backgroundColor: color1 }}
-        ></Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-      </ul>
-    </>
+    <ul
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        width: "600px",
+        margin: "60px auto 0",
+        cursor: "pointer",
+      }}
+    >
+      <Box
+        className={"name"}
+        onClick={() => onChangeColor1()}
+        style={{ backgroundColor: color }}
+      ></Box>
+    </ul>
   );
 }
--- src/pages/practice-04/Chat.js
+++ src/pages/practice-04/Chat.js
@@ -1,3 +1,171 @@
+import {
+  useState,
+  createContext,
+  useContext,
+  useId,
+  useEffect,
+  useRef, //state가 변경되어 다시 돌더라도 초기화하지 않음, 값 유지 o, 렌더링 x
+} from "react";
+
+const ChatContext = createContext(null);
+
 export default function Chat() {
-  return <></>;
+  const [message, setMessage] = useState([]);
+  return (
+    <>
+      <ChatContext.Provider value={{ message, setMessage }}>
+        <ChatUser1 />
+        <ChatUser2 />
+        {/* <ChatUser3 /> */}
+      </ChatContext.Provider>
+    </>
+  );
 }
+
+function ChatUser1() {
+  const id = useId();
+  const context = useContext(ChatContext);
+  const [input, setInput] = useState("");
+  // const onChange = (event) => {
+  //   context?.setMessage(event.target.value);
+  // };
+  useEffect(() => {
+    console.log(context?.message);
+  }, [context?.message]);
+
+  const onKeyDown = (event) => {
+    if (event.keyCode === 13) {
+      console.log("key event", event.target.value);
+      const date = new Date();
+      context?.setMessage((message) => {
+        return [
+          ...message,
+          {
+            id,
+            message: event.target.value,
+            date: new Intl.DateTimeFormat("ko-KR").format(date),
+          },
+        ];
+        // return {
+        //   ...message,
+        //   [id]: [...(message[id] ?? []), event.target.value],
+        //   //a ?? b : a가 null, undefined면 b를 반환 아니면 a를 반환
+        //   //null, undefined만 체크
+        // };
+      });
+      setInput("");
+    }
+  };
+
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const onDelete = (index) => {
+    console.log("deleteindex", index);
+    context?.setMessage((message) => {
+      const msg = [...message];
+      msg.splice(index, 1);
+      return msg;
+    });
+  };
+  return (
+    <>
+      <ul>
+        {context?.message.map((msg, index) => (
+          <li key={index}>
+            <div>
+              {msg.id}: {msg.message}
+              {id === msg.id && (
+                <button onClick={() => onDelete(index)}>X</button>
+              )}
+            </div>
+            <div>{msg.date}</div>
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        onKeyDown={onKeyDown}
+        value={input}
+        onChange={onChange}
+      />
+    </>
+  );
+}
+
+function ChatUser2() {
+  const id = useId();
+  const context = useContext(ChatContext);
+  const [input, setInput] = useState("");
+  // const onChange = (event) => {
+  //   context?.setMessage(event.target.value);
+  // };
+  const onKeyDown = (event) => {
+    if (event.keyCode === 13) {
+      console.log("key event", event.target.value);
+      context?.setMessage((message) => {
+        return [...message, { id, message: event.target.value }];
+        // return {
+        //   ...message,
+        //   [id]: [...(message[id] ?? []), event.target.value],
+        //   //a ?? b : a가 null, undefined면 b를 반환 아니면 a를 반환
+        //   //null, undefined만 체크
+        // };
+      });
+      setInput("");
+    }
+  };
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+  const onDelete = (index) => {
+    console.log("index", index);
+  };
+
+  return (
+    <>
+      <ul>
+        {context?.message.map((msg, index) => (
+          <li key={index}>
+            {msg.id}: {msg.message}
+            {id === msg.id && (
+              <button onClick={() => onDelete(index)}>X</button>
+            )}
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        onKeyDown={onKeyDown}
+        value={input}
+        onChange={onChange}
+      />
+    </>
+  );
+}
+
+// function ChatUser3() {
+//   const context = useContext(ChatContext);
+//   const onChange = (event) => {
+//     context?.setMessage(event.target.value);
+//   };
+//   if (!context)
+//     return (
+//       <>
+//         <div>not found context</div>
+//       </>
+//     );
+//   return (
+//     <>
+//       <ul>
+//         <li></li>
+//       </ul>
+//       <input type="text" value={context.message} onChange={onChange} />
+//     </>
+//   );
+// }
+
+//채팅창 스타일 (emotion)
+//date를써서 입력된 date값 현재 날짜 대비 10초가 지나야 지울 수 있음 //안지났으면 aleat ondelete때 비교
+//date찾아서! 날짜정보 비교
